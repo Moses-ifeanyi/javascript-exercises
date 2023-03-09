8a256d147b65a9bc838fec63ee9b685095937aa9
Moses-ifeanyi <moses-ifeanyim@gmail.com>
2023-03-09 01:19:03 +0000

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
-
+const reverseString = (string) =>{
+    let reverse = [...string].reverse().join("");
+    return reverse;
+}
 // Do not edit below this line
 module.exports = reverseString;
